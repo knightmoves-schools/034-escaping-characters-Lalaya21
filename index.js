84f60b15390a3ecd3b5c94bbcfff32dbdff75c3a
lalaya21 <lalaya.shuckahosee@students.msswarriors.org>
2026-02-17 19:57:41 +0000

    done3

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 var introduction = "introduction\n\t sigma 'alpha' \"beta\" \\ what the sigma";
-document.getElementById('result').innerHTM L= introduction;
+document.getElementById('result').innerHTML= introduction;
